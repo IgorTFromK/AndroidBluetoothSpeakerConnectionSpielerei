9307c8cee8a442a68bb8f7305119741fb074e96e
IgorTFromK <Igortkatschenko@yahoo.de>
2021-04-22 17:07:57 +0200

    init oscilator class

---
+++ app/src/main/java/com/example/bluetoothspeakerconnectionspielerei/Oscilator.kt
@@ -0,0 +1,9 @@
+package com.example.bluetoothspeakerconnectionspielerei
+
+//Todo:Fertig maken
+class Oscilator(val sampleRate:Int, val channelSize: Int, val frequency:Int ){
+    fun render(): Array<Int> {
+        Math.PI
+        return arrayOf(10)
+    }
+}
