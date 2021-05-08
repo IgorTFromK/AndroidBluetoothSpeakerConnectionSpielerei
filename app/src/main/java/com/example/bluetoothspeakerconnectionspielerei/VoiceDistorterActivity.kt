2a002208d5c02bd8c61f0c5e5dc27866a8439973
IgorTFromK <Igortkatschenko@yahoo.de>
2021-05-09 00:58:09 +0200

    toggle button

--- app/src/main/java/com/example/bluetoothspeakerconnectionspielerei/VoiceDistorterActivity.kt
+++ app/src/main/java/com/example/bluetoothspeakerconnectionspielerei/VoiceDistorterActivity.kt
@@ -1,17 +1,33 @@
 package com.example.bluetoothspeakerconnectionspielerei
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
 class VoiceDistorterActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var mRecordingButton: ToggleButton
+
+    companion object {
+        private  val LOG_TAG = "VoiceDistorterActivity: "
+    }
+
+
     override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
         setContentView(R.layout.voice_distorter_activity)
+
+        mRecordingButton = findViewById(R.id.toggleButton)
+        mRecordingButton.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        if(mRecordingButton.isChecked){
+            Log.d(LOG_TAG, "Recording wird gestartet !")
+        } else{
+            Log.d(LOG_TAG, "Recording wird beendet !");
+        }
     }
 }
